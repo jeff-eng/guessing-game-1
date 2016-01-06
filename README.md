7f41cc6d3718e59158119164996fb0c55cf5b474
Frazier Mork <fraziermork@gmail.com>
2016-01-06 13:57:13 -0800

    Realized that I had forgotten to add the counter for the number of questions answered correctly. Updated the README file.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # guessing-game
-Homework assignment for codefellows 201 day 2
+Homework assignment for codefellows 201 day 3
+
+This game consists of 2 parts: a series of verbal questions and a number-guessing game.
+
+For the verbal portion, questions are stored in an array that contains each question. Questions inside that are formatted like [question, answer, response if correct, response if incorrect]. A for loop iterates through the array that contains the questions and determines whether the user's answer is consistent with the second element of each question array. If it is, it displays the 'response if correct', otherwise it displays the 'response if incorrect'.
+
+The second portion of the game is contained in a function called numberGuess that generates a number at random between 1 and 100 and prompts the user to guess it. Then, it calls a second function, checkAnswer, inside a while loop. The function checkAnswer  determines whether the number was equal to, greater than, or less than the random number--if it is equal, it returns false to break the loop, otherwise it returns true to keep the loop going.
+
+Current problems include, but are likely not limited to:
+It would be more elegant if the verbal portion of the game were contained within a function as well.
+There is no current counter for the number of correct vs. incorrect answers.
+In the while loop, it calls checkAnswer twice to get the two elements of the array it returns, which isn't necessary.
+The question list for the verbal portion seems pretty clunky. 
--- app.js
+++ app.js
@@ -1,39 +1,44 @@
 var userName = prompt("What is your name?");
-var question1 = ['Is my first name Frazier?', 'y',"That's right! My name is Frazier! Good job " + userName +'!', "Nope! My name actually is Frazier. Better luck next time " + userName +'.'];
-var question2 = ['Was I born in Washington?', 'n', "That's right! I was actually born in New York. Good job " + userName +'!', "Nope! I was actually born in New York. Better luck next time " + userName +'.'];
-var question3 =['Is my favorite color green?', 'y', "That's right! My favorite color is green. Good job " + userName +'!', "Nope! My favorite color actually is green. Better luck next time " + userName +'.'];
-var questionList = [question1, question2, question3];
-var correctAnsCount = 0;
-var thisQuestion;
-var userAnswer;
 
 
 //Iterates through the questions, determining whether the user answered the question correctly or not
-for (i=0; i<questionList.length; i++){
-  thisQuestion = questionList[i];
-  console.log(thisQuestion);
-  userAnswer = prompt(thisQuestion[0]).toLowerCase();
-  if (userAnswer === 'yes') {
-    userAnswer = 'y';
-  } else if (userAnswer === 'no') {
-    userAnswer = 'n';
-  }
+var guessingGame = function(){
+  var question1 = ['Is my first name Frazier?', 'y',"That's right! My name is Frazier! Good job " + userName +'!', "Nope! My name actually is Frazier. Better luck next time " + userName +'.'];
+  var question2 = ['Was I born in Washington?', 'n', "That's right! I was actually born in New York. Good job " + userName +'!', "Nope! I was actually born in New York. Better luck next time " + userName +'.'];
+  var question3 =['Is my favorite color green?', 'y', "That's right! My favorite color is green. Good job " + userName +'!', "Nope! My favorite color actually is green. Better luck next time " + userName +'.'];
+  var questionList = [question1, question2, question3];
+  var correctAnsCount = 0;
+  var thisQuestion;
+  var userAnswer;
 
-  if (userAnswer === 'y' || userAnswer === 'n') {
-    if (userAnswer === thisQuestion[1]){
-      console.log(thisQuestion[2]);
-      alert(thisQuestion[2]);
-    } else {
-      console.log(thisQuestion[3]);
-      alert(thisQuestion[3]);
+  for (i=0; i<questionList.length; i++){
+    thisQuestion = questionList[i];
+    console.log(thisQuestion);
+    userAnswer = prompt(thisQuestion[0]).toLowerCase();
+    if (userAnswer === 'yes') {
+      userAnswer = 'y';
+    } else if (userAnswer === 'no') {
+      userAnswer = 'n';
     }
-  } else {
-    alert('Critical error! Enter yes or no next time!');
+
+    if (userAnswer === 'y' || userAnswer === 'n') {
+      if (userAnswer === thisQuestion[1]){
+        console.log(thisQuestion[2]);
+        alert(thisQuestion[2]);
+      } else {
+        console.log(thisQuestion[3]);
+        alert(thisQuestion[3]);
+      }
+    } else {
+      alert('Critical error! Enter yes or no next time!');
+    }
   }
+
 }
+guessingGame();
 
 
-//This will check whether the answer is right or not
+//This will check whether the guessed number is right or not
 var checkAnswer = function(myGuess, numberToGuess){
   //console.log('The checkAnswer function was called');
   //console.log('The number to guess is ' + numberToGuess);
@@ -57,6 +62,7 @@ var checkAnswer = function(myGuess, numberToGuess){
 //This will run the guess a number game
 var numberGuess = function (){
   var userGuess;
+  var checkAnswerResult;
   var numToGuess = Math.floor(Math.random()*100 + 1);
   var whetherIncorrect = true;
   var promptString = 'Guess a number between 1 and 100';
