4238d025c2a339dec7682c6371b466e8ad3302fc
Frazier Mork <fraziermork@gmail.com>
2016-01-07 12:33:03 -0800

    updated guessing-game to requirements for homework for class 3 so class 4 homework could be done with pair programming. Ignore wednesday branch

--- app.js
+++ app.js
@@ -1,38 +1,73 @@
 var userName = prompt("What is your name?");
+var counter = 0;
 
-var ans1 = prompt("What is my first name?");
+var ans1 = prompt("Is my first name Frazier?").toLowerCase();
+if (ans1 === 'yes'){
+  ans1 = 'y';
+} else if (ans1 === 'no'){
+  ans1 = 'n';
+}
 console.log("User answer to Q1: " + ans1);
-if (ans1.toLowerCase() === "frazier"){
+if (ans1 === "y"){
   alert("Good job " + userName + "!");
+  counter++;
 } else {
-  alert("Wrong! My name is Frazier, not " + ans1 + ". Remember that " + userName);
+  alert("Wrong! My name actually is Frazier. Remember that " + userName);
 }
 
-var ans2 = prompt("What is my favorite color?");
+
+
+var ans2 = prompt("Is my favorite color blue?").toLowerCase();
+if (ans2 === 'yes'){
+  ans2 = 'y';
+} else if (ans1 === 'no'){
+  ans2 = 'n';
+}
 console.log("User answer to Q2: "+ ans2);
-if (ans2.toLowerCase() === "green"){
-  alert("Good job " + userName + "!");
+if (ans2 === 'n'){
+  alert("Good job " + userName + "! My favorite color is green.");
+  counter++;
 } else {
-  alert("Wrong! My favorite color is green, not " + ans2 + ". Remember that " + userName);
+  alert("Wrong! My favorite color is green. Remember that " + userName);
 }
 
-var ans3 = prompt("What state was I born in?");
+
+
+var ans3 = prompt("Was I born in Washington?").toLowerCase();
+if (ans3 === 'yes'){
+  ans3 = 'y';
+} else if (ans1 === 'no'){
+  ans3 = 'n';
+}
 console.log("User answer to Q3: "+ ans3);
-if (ans3.toLowerCase() === "new york"){
-  alert("Good job " + userName + "!");
+if (ans3 === "n"){
+  alert("Good job " + userName + "! I was born in New York.");
+  counter++;
 } else {
-  alert("Wrong! I was born in New York, not " + ans3 + ". Remember that " + userName);
+  alert("Wrong! I was born in New York. Remember that " + userName);
 }
 
-var ans4 = Number(prompt("What number between 1 and 5 am I thinking of?"));
+
+
+
+var guessed = false;
+var ans4;
 var numToGuess = Math.floor(Math.random()*5 + 1);
 console.log("User answer to Q4: "+ ans4);
-if (ans4 === numToGuess){
-  alert("Good job " + userName + "!");
-} else if(ans4 >= numToGuess) {
-  alert("Too high " + userName + "! I was thinking of " + numToGuess + ".");
-} else if (ans4 <= numToGuess) {
-  alert("Too low " + userName + "! I was thinking of " + numToGuess + ".");
-} else {
-  alert("Critical error!! " + userName + " evacuate immediately!");
-} 
+while (guessed === false) {
+  ans4 = Number(prompt("What number between 1 and 5 am I thinking of?"));
+
+  if (ans4 === numToGuess){
+    alert("Good job " + userName + "!");
+    counter++;
+    guessed = true;
+  } else if(ans4 >= numToGuess) {
+    alert("Too high " + userName + "! Guess again.");
+  } else if (ans4 <= numToGuess) {
+    alert("Too low " + userName + "! Guess again.");
+  } else {
+    alert("Critical error!! " + userName + " evacuate immediately!");
+  }
+};
+
+alert('Congratulations ' + userName + ' you got ' + counter + ' questions correct!');
